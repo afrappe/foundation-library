a393ef2c9307cae6d8b6a02006efcf796dc3badc
Arturo Frappé Muñoz <afrappe@aineas.consulting>
2026-02-01 13:27:40 -0600

    Actualiza plugin de gradle  y resuelve los problemas asociados a esta actualización

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    kotlin("kapt")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -52,10 +51,6 @@ android {
         compose = true
         viewBinding = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.6"
     }
 
     packaging {
